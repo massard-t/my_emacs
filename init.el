91ee8b035ca8e455b4997965944d4f788113a8d9
Theo Massard <massar_t@etna-alternance.net>
2016-08-09 11:52:19 +0200

    Update emacs (/root -> ~)

--- init.el
+++ init.el
@@ -10,23 +10,23 @@
   (package-initialize))
 
 ;; dockerfile mode with syntax ;;
-(add-to-list 'load-path "/root/.emacs.d/dockerfile-mode")
+(add-to-list 'load-path "~/.emacs.d/dockerfile-mode")
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
 ;; neotree, [f8] to see current directory branch ;;
-(add-to-list 'load-path "/root/.emacs.d/emacs-neotree")
+(add-to-list 'load-path "~/.emacs.d/emacs-neotree")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
 
 ;; powerline ;;
-(add-to-list 'load-path "/root/.emacs.d/powerline")
+(add-to-list 'load-path "~/.emacs.d/powerline")
 (require 'powerline)
 (powerline-default-theme)
 
 ;; moe-theme ;;
 (add-to-list 'custom-theme-load-path "~/.emacs.d/moe-theme.el/")
-(add-to-list 'load-path "/root/.emacs.d/moe-theme.el")
+(add-to-list 'load-path "~/.emacs.d/moe-theme.el")
 (require 'moe-theme)
 (load-theme 'moe-dark t)
 (moe-dark)
@@ -40,8 +40,8 @@
 (ac-config-default)
 
 ;; latex config ;;
-(add-to-list 'load-path "/root/.emacs.d/math-symbol-lists")
-(add-to-list 'load-path "/root/.emacs.d/ac-math/")
+(add-to-list 'load-path "~/.emacs.d/math-symbol-lists")
+(add-to-list 'load-path "~/.emacs.d/ac-math/")
 (require 'ac-math)
 (add-to-list 'ac-modes 'latex-mode) ; make autocomplete aware of latex-mode
 
@@ -55,14 +55,14 @@
 (setq ac-math-unicode-in-math-p t)
 
 ;; go-mode ;;
-(add-to-list 'load-path "/root/.emacs.d/go-mode.el")
+(add-to-list 'load-path "~/.emacs.d/go-mode.el")
 (require 'go-mode-autoloads)
 
 ;; yasnippet ;;
 (yas-global-mode 1)
 
 ;; git-modes ;;
-(add-to-list 'load-path "/root/.emacs.d/git-modes")
+(add-to-list 'load-path "~/.emacs.d/git-modes")
 (require 'git-modes)
 
 ;; saveplace puts the cursos back to its ;;
@@ -83,3 +83,6 @@
 (setq column-number-mode t) ; show the column number on the powerline (bot-right)
 (show-paren-mode 1) ; highlights matching [] () 
 (electric-pair-mode 1) ; creates pairs when working typing '[' or '('
+(eval-after-load 'org-mode
+  '(define-key org-mode-map ["C-1"] 'org-todo))
+;;(global-set-key (kbd "C-1") org-todo) ; Maps Ctrl+1 to TODO
